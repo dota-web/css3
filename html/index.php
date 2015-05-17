d370aabcd169c458c2a112b636c1ec114636281c
hexing <850393171@qq.com>
2015-05-17 11:43:18 +0800

    css init

---
+++ html/index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<div class="head"></div>
+<div class="main"></div>
+<div class="foot"></div>
+</body>
+</html>
